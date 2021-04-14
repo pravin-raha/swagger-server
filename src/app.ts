6c1c744a3fc038ec5bdd213629c711cb560f9d0f
Pravin Rahangdale <pravinrahangdale41@gmail.com>
2021-04-15 00:21:32 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
+import * as bodyParser from 'body-parser'
+
+class App {
+  private httpServer: any
+
+  constructor() {
+    this.httpServer = express()
+
+    this.httpServer.use(bodyParser.urlencoded({ extended: true }));
+    this.httpServer.use(bodyParser.json());
+    
+
+    this.httpServer.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));     
+  }
+
+  public Start = (port: number) => {
+    return new Promise((resolve, reject) => {
+
+      this.httpServer.listen(
+        port,
+        () => {
+          resolve(port)
+        })
+        .on('error', (err: object) => reject(err));
+    })
+  }
+}
+
+export default App;
